9e83875ce731e3a386ee9b9944b46464086cb574
shifenhutu <104620424+shifenhutu@users.noreply.github.com>
2025-11-28 19:51:42 +0800

    chore(version): 更新版本号到0.1.2
    - 将__version__从0.1.0更新至0.1.2
    - 在pyproject.toml中同步更新项目版本号到0.1.2

--- django_admin_advanced_search/__init__.py
+++ django_admin_advanced_search/__init__.py
@@ -3,7 +3,7 @@
 This package provides advanced search functionality for Django Admin interface.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.2'
 __author__ = 'shifenhutu'
 __email__ = 'shifenhutu@example.com'
 __license__ = 'MIT'
