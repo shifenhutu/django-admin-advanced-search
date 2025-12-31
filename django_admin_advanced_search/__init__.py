34639ac2968c623018cae2d38e9e947927d04586
shifenhutu <104620424+shifenhutu@users.noreply.github.com>
2026-01-01 01:53:20 +0800

    docs(docs): 添加文档

--- django_admin_advanced_search/__init__.py
+++ django_admin_advanced_search/__init__.py
@@ -3,6 +3,7 @@
 This package provides advanced search functionality for Django Admin interface.
 """
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = 'shifenhutu'
 __license__ = 'MIT'
+__doc__ = "https://github.com/shifenhutu/django-admin-advanced-search"
